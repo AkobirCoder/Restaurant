9e08ac7f9705a6735cd2a95637beabae715f7be9
AkobirCoder <akobir0308@gmail.com>
2025-09-04 15:37:03 +0500

    eslint.config.js was updated

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,20 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "modul" } },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      sourceType: "module",   // Hamma fayl uchun import/export ishlaydi
+      globals: globals.browser,
+    },
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["server-*.js"],
+    languageOptions: {
+      sourceType: "module",   // commonjs emas, modul sifatida ishlatamiz
+      globals: globals.node,
+    },
+  },
 ]);
