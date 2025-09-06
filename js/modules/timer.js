b1fe3b84fec42f2060bae82354c2f1a2e81b6478
AkobirCoder <akobir0308@gmail.com>
2025-09-06 12:28:34 +0500

    ./js/modules/timer.js was added

---
+++ js/modules/timer.js
@@ -0,0 +1,68 @@
+function __Timer(deadline, selector) {
+
+    // const deadline = '2025-09-08';
+
+    function getTimeRemainer(endDate) {
+        const time = Date.parse(endDate) - Date.parse(new Date());
+
+        let days, hours, minutes, seconds;
+
+        if (time <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(time / (1000 * 60 * 60 * 24));
+            hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+            minutes = Math.floor((time / (1000 * 60)) % 60);
+            seconds = Math.floor((time / 1000) % 60);
+        }
+
+        return {
+            totalTime: time,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+        }
+    }
+
+    function formatTimeDigit(timeDigit) {
+        if (timeDigit >= 0 && timeDigit < 10) {
+            return `0${timeDigit}`;
+        } else {
+            return timeDigit;
+        }
+    }
+
+    function setClock(selector, endDate) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeIntervalId = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const time = getTimeRemainer(endDate);
+
+            days.textContent = formatTimeDigit(time.days);
+            hours.textContent = formatTimeDigit(time.hours);
+            minutes.textContent = formatTimeDigit(time.minutes);
+            seconds.textContent = formatTimeDigit(time.seconds);
+
+            if (time.totalTime <= 0) {
+                clearInterval(timeIntervalId);
+            }
+        }
+    }
+
+    // setClock('.timer', deadline);
+
+    setClock(selector, deadline);
+}
+
+export default __Timer;
