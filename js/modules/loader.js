38fc3a5849a81051f044e898deed985a45570653
AkobirCoder <akobir0308@gmail.com>
2025-09-13 10:41:04 +0500

    ./js/modules was deleted

--- js/modules/loader.js
+++
@@ -1,9 +0,0 @@
-function __Loader(loaderSelector) {
-    const loaderWrapper = document.querySelector(loaderSelector);
-
-    setTimeout(() => {
-        loaderWrapper.style.display = 'none';
-    }, 1000);
-}
-
-export default __Loader;
