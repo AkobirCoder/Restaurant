b37abede878b4e21a67aa4eeaec27305594119f3
AkobirCoder <akobir0308@gmail.com>
2025-09-13 16:46:05 +0500

    eslint.config.mjs was added

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: {
+      sourceType: "module", // import/export ishlaydi
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+]);
+
+
+
